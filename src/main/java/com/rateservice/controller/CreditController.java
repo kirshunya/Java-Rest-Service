374dff92496b8fe4ec8473ee9f924ac698664ed3
Nikita Bakunovich <112703570+kirshunya@users.noreply.github.com>
2024-03-27 13:12:20 +0300

    feat: Swagger and CheckStyle.

--- src/main/java/com/rateservice/controller/CreditController.java
+++ src/main/java/com/rateservice/controller/CreditController.java
@@ -14,27 +14,43 @@ public class CreditController {
     private final CreditService creditService;
 
     @GetMapping("/all")
-    public List<Credit> getAllCredits() { return creditService.getAllCredits(); }
+    public List<Credit> getAllCredits() {
+        return creditService.getAllCredits();
+    }
 
     @PostMapping("/save_credit")
-    public Credit saveCredit(@RequestBody Credit credit) { return creditService.saveCredit(credit);}
+    public Credit saveCredit(@RequestBody Credit credit) {
+        return creditService.saveCredit(credit);
+    }
 
     @PutMapping("/{id}")
-    Credit updateCredit(@PathVariable Long id, @RequestBody Credit newCredit) { return creditService.updateCredit(id, newCredit);}
+    Credit updateCredit(@PathVariable Long id, @RequestBody Credit newCredit) {
+        return creditService.updateCredit(id, newCredit);
+    }
 
     @DeleteMapping("/delete/{id}")
-    void deleteCredit(@PathVariable Long id) { creditService.deleteCredit(id); }
+    void deleteCredit(@PathVariable Long id) {
+        creditService.deleteCredit(id);
+    }
 
     @GetMapping("/find/{id}")
-    Credit getCreditById(@PathVariable Long id) { return creditService.findCreditById(id); }
+    Credit getCreditById(@PathVariable Long id) {
+        return creditService.findCreditById(id);
+    }
 
     @PostMapping("/add_user_by_id/{crId}")
-    Credit addUserById(@PathVariable Long crId, @RequestParam Long usId) { return creditService.addCreditToUserById(crId, usId); }
+    Credit addUserById(@PathVariable Long crId, @RequestParam Long usId) {
+        return creditService.addCreditToUserById(crId, usId);
+    }
 
     @PostMapping("/add_user/{usId}")
-    Credit addUser(@PathVariable Long usId, @RequestBody Credit credit) { return creditService.addCreditToUser(credit, usId);}
+    Credit addUser(@PathVariable Long usId, @RequestBody Credit credit) {
+        return creditService.addCreditToUser(credit, usId);
+    }
 
     @DeleteMapping("/pay_credit/{crId}")
-    String payCredit(@PathVariable Long crId, @RequestParam Long cardId) { return creditService.creditPay(crId, cardId);}
+    String payCredit(@PathVariable Long crId, @RequestParam Long cardId) {
+        return creditService.creditPay(crId, cardId);
+    }
 
 }
